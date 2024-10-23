0fb126d7e862d74eab0da0b704b7227edf9f2dee
Gustavo Andrade <andradegustavo2745@gmail.com>
2024-10-23 18:08:21 -0300

    arvore.c corrigido

--- TRABALHO/arvore.c
+++ TRABALHO/arvore.c
@@ -1,57 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "arvore.h"
 
-// Estrutura para um nó da árvore binária não balanceada
-struct no {
-    int chave;
-    struct no* esquerdo;
-    struct no* direito;
-};
 
-// Função para criar um novo nó e alocar espaço 
-struct no* novono(int chave) {
-    struct no* no = (struct no*)malloc(sizeof(struct no));
+// Função para criar um novo nó e alocar espaço
+struct no *novono(int chave)
+{
+    struct no *no = (struct no *)malloc(sizeof(struct no));
     no->chave = chave;
     no->esquerdo = no->direito = NULL;
     return no;
 }
 
 // Função para inserir um nó na árvore binária não balanceada
-struct no* inserirNbalanceada(struct no* no, int chave) {
-    if (no == NULL) {
+struct no *inserirNbalanceada(struct no *no, int chave)
+{
+    if (no == NULL)
+    {
         return novono(chave);
     }
-    if (chave < no->chave) {
+    if (chave < no->chave)
+    {
         no->esquerdo = inserirNbalanceada(no->esquerdo, chave);
-    } else {
+    }
+    else
+    {
         no->direito = inserirNbalanceada(no->direito, chave);
     }
     return no;
 }
 
-// Estrutura para um nó da árvore AVL(balanceada)
-struct AVLno {
-    int chave;
-    struct AVLno* esquerdo;
-    struct AVLno* direito;
-    int altura;
-};
+
 
 // Função para obter a altura de um nó
-int altura(struct AVLno* N) {
-    if (N == NULL) return 0;
+int altura(struct AVLno *N)
+{
+    if (N == NULL)
+        return 0;
     return N->altura;
 }
 
 // Função para obter o máximo entre dois números
-int max(int a, int b) {
+int max(int a, int b)
+{
     return (a > b) ? a : b;
 }
 
 // Função para criar um novo nó da árvore AVL
-struct AVLno* noAVLno(int chave) {
-    struct AVLno* no = (struct AVLno*)malloc(sizeof(struct AVLno));
+struct AVLno *noAVLno(int chave)
+{
+    struct AVLno *no = (struct AVLno *)malloc(sizeof(struct AVLno));
     no->chave = chave;
     no->esquerdo = no->direito = NULL;
     no->altura = 1; // novo nó é inicialmente adicionado à altura 1
@@ -59,9 +58,10 @@ struct AVLno* noAVLno(int chave) {
 }
 
 // Função para realizar uma rotação à direita
-struct AVLno* direitorotacao(struct AVLno* y) {
-    struct AVLno* x = y->esquerdo;
-    struct AVLno* T2 = x->direito;
+struct AVLno *direitorotacao(struct AVLno *y)
+{
+    struct AVLno *x = y->esquerdo;
+    struct AVLno *T2 = x->direito;
 
     // Realizar rotação
     x->direito = y;
@@ -75,9 +75,10 @@ struct AVLno* direitorotacao(struct AVLno* y) {
 }
 
 // Função para realizar uma rotação à esquerda
-struct AVLno* esquerdorotacao(struct AVLno* x) {
-    struct AVLno* y = x->direito;
-    struct AVLno* T2 = y->esquerdo;
+struct AVLno *esquerdorotacao(struct AVLno *x)
+{
+    struct AVLno *y = x->direito;
+    struct AVLno *T2 = y->esquerdo;
 
     // Realizar rotação
     y->esquerdo = x;
@@ -91,18 +92,25 @@ struct AVLno* esquerdorotacao(struct AVLno* x) {
 }
 
 // Função para obter o fator de balanceamento
-int balanceamento(struct AVLno* N) {
-    if (N == NULL) return 0;
+int balanceamento(struct AVLno *N)
+{
+    if (N == NULL)
+        return 0;
     return altura(N->esquerdo) - altura(N->direito);
 }
 
 // Função para inserir um nó na árvore AVL
-struct AVLno* insereAVL(struct AVLno* no, int chave) {
-    if (no == NULL) return noAVLno(chave);
+struct AVLno *insereAVL(struct AVLno *no, int chave)
+{
+    if (no == NULL)
+        return noAVLno(chave);
 
-    if (chave < no->chave) {
+    if (chave < no->chave)
+    {
         no->esquerdo = insereAVL(no->esquerdo, chave);
-    } else {
+    }
+    else
+    {
         no->direito = insereAVL(no->direito, chave);
     }
 
@@ -115,23 +123,27 @@ struct AVLno* insereAVL(struct AVLno* no, int chave) {
     // Se o nó se tornar desbalanceado, existem 4 casos
 
     // Caso à esquerda à esquerda
-    if (balance > 1 && chave < no->esquerdo->chave) {
+    if (balance > 1 && chave < no->esquerdo->chave)
+    {
         return direitorotacao(no);
     }
 
     // Caso à direita à direita
-    if (balance < -1 && chave > no->direito->chave) {
+    if (balance < -1 && chave > no->direito->chave)
+    {
         return esquerdorotacao(no);
     }
 
     // Caso à esquerda à direita
-    if (balance > 1 && chave > no->esquerdo->chave) {
+    if (balance > 1 && chave > no->esquerdo->chave)
+    {
         no->esquerdo = esquerdorotacao(no->esquerdo);
         return direitorotacao(no);
     }
 
     // Caso à direita à esquerda
-    if (balance < -1 && chave < no->direito->chave) {
+    if (balance < -1 && chave < no->direito->chave)
+    {
         no->direito = direitorotacao(no->direito);
         return esquerdorotacao(no);
     }
@@ -139,13 +151,15 @@ struct AVLno* insereAVL(struct AVLno* no, int chave) {
     return no; // Retornar o ponteiro do nó (inalterado)
 }
 
-int main() {
-    struct no* arvoreNbalanceada = NULL; // Raiz da árvore não balanceada
-    struct AVLno* arvoreAVL = NULL; // Raiz da árvore AVL
+int main()
+{
+    struct no *arvoreNbalanceada = NULL; // Raiz da árvore não balanceada
+    struct AVLno *arvoreAVL = NULL;      // Raiz da árvore AVL
 
     // Abrindo o arquivo para leitura
-    FILE* file = fopen("teste1.txt", "r");//trocar o nome do file para teste2,teste3 ou testecurto para ler o arquivo diferente
-    if (file == NULL) {
+    FILE *file = fopen("teste1.txt", "r"); // trocar o nome do file para teste2,teste3 ou testecurto para ler o arquivo diferente
+    if (file == NULL)
+    {
         printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
@@ -156,9 +170,11 @@ int main() {
 
     // Inserindo na árvore não balanceada
     start = clock();
-    for (int i = 0; i < 20000; i++) { // Repetir 20000 vezes para tamanho maximo to arquivo de entrada, para testes diminuir o loop
+    for (int i = 0; i < 20000; i++)
+    {                 // Repetir 20000 vezes para tamanho maximo to arquivo de entrada, para testes diminuir o loop
         rewind(file); // Reabrir o arquivo para cada iteração
-        while (fscanf(file, "I %d", &chave) == 1) {
+        while (fscanf(file, "I %d", &chave) == 1)
+        {
             arvoreNbalanceada = inserirNbalanceada(arvoreNbalanceada, chave);
         }
     }
@@ -171,9 +187,11 @@ int main() {
 
     // Inserindo na árvore AVL
     start = clock();
-    for (int i = 0; i < 20000; i++) { // Repetir 20000 vezes para tamanho maximo to arquivo de entrada, para testes diminuir o loop
+    for (int i = 0; i < 20000; i++)
+    {                 // Repetir 20000 vezes para tamanho maximo to arquivo de entrada, para testes diminuir o loop
         rewind(file); // Reabrir o arquivo para cada iteração
-        while (fscanf(file, "I %d", &chave) == 1) {
+        while (fscanf(file, "I %d", &chave) == 1)
+        {
             arvoreAVL = insereAVL(arvoreAVL, chave);
         }
     }
